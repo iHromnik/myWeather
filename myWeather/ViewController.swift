3a502b1074cc6bc73ec5991530262f0f929d2229
Roman Khovalko <hromnik@me.com>
2024-02-08 10:12:42 +0200

    Initial Commit

---
+++ myWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myWeather
+//
+//  Created by iHRomnik on 08.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ myWeatherUITests/myWeatherUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  myWeatherUITestsLaunchTests.swift
+//  myWeatherUITests
+//
+//  Created by iHRomnik on 08.02.2024.
+//
+
+import XCTest
+
+final class myWeatherUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
